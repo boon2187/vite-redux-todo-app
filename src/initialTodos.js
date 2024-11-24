d48e890425ded597d4e85fbf21435b1ff0290bd9
boon2187 <fumiya.mai.ito@gmail.com>
2024-11-24 21:37:27 +0900

    最初に表示したり操作するTodoリストを作成

---
+++ src/initialTodos.js
@@ -0,0 +1,7 @@
+const initTodos = [
+  { id: 1, text: "Buy milk", isCompleted: false },
+  { id: 2, text: "Meeting with Ali", isCompleted: false },
+  { id: 3, text: "Go to gym", isCompleted: false },
+];
+
+export default initTodos;

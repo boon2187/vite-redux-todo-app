b3d8f8f24efc42964b8547e03e32d886baa4c2b5
boon2187 <fumiya.mai.ito@gmail.com>
2024-11-24 22:35:13 +0900

    削除ボタンを設置し、onClickイベントにdeleteTodoのreducerを適用

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Form from "./conponents/Form";
+import { deleteTodo } from "./features/todos/TodosSlice";
 
 function App() {
   const { todos } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
   return (
     <>
       <Form />
       {todos.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
+        <div key={todo.id}>
+          <p>{todo.text}</p>
+          <button onClick={() => dispatch(deleteTodo(todo.id))}>削除</button>
+        </div>
       ))}
     </>
   );

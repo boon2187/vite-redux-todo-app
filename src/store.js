5282dadfc6329d2459b2b2fdc1101b6676e3f65c
boon2187 <fumiya.mai.ito@gmail.com>
2024-11-24 21:48:29 +0900

    storeのreducerの実装の間違いを修正

--- src/store.js
+++ src/store.js
@@ -2,5 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./features/todos/TodosSlice";
 
 export const store = configureStore({
-  reducer: todoReducer,
+  reducer: {
+    todos: todoReducer,
+  },
 });
